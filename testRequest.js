33e3d1c8d813be884ac2739dd0aae71cd974ed29
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2024-04-07 14:25:29 +0200

    automatisation de la recupération et utilisation du JWT

--- testRequest.js
+++ testRequest.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const dotenv = require('dotenv');
+const fs = require('fs');
+const { log } = require('console');
 dotenv.config();
 
 /*
@@ -16,21 +18,26 @@ Et supprimer la ligne AUthorization dans headers pour les routes qui ne nécessi
 */
 
 function testRequest() {
-    const url = 'http://localhost' + process.env.LISTENING_PORT + '/defis/66024ff027b3edb1aa2c17b1';
+    const url = 'http://localhost:' + process.env.LISTENING_PORT + '/login/';
 
     const data = {
-        participants_max: 5,
+        participants_max: 4,
         participants_actuels: 3,
         technologies: ['Unity', 'C#'],
     };
 
-    const options = {
-        method: 'PUT',
+    let options = {
+        method: 'GET',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + process.env.TEMPORARY_JWT,
         }
     };
+
+    try {
+        options = CheckIfTokenSaved(options);
+    } catch (error) {
+        console.log("Erreur lors de la récupération du token JWT: " + error);
+    }
 
     const req = http.request(url, options, (res) => {
         let responseData = '';
@@ -41,11 +48,45 @@ function testRequest() {
 
         res.on('end', () => {
             console.log(responseData);
+            CheckIfTokenReceived(url, responseData);
         });
     });
 
     req.write(JSON.stringify(data));
     req.end();
 }
+
+function CheckIfTokenReceived(url, responseData) {
+    if (!/\/login/.test(url)) return;
+
+    try {
+        //Si on est sur la route /login, on récupère le token JWT et on le sauvegarde dans un fichier txt
+        fs.writeFileSync('jwt.txt', responseData, (err) => {
+            if (err) throw err;
+            console.log('JWT sauvegardé dans jwt.txt');
+        });
+
+    } catch (error) {
+        console.log("Erreur lors de la sauvegarde du token JWT: " + error);
+    }
+}
+
+
+function CheckIfTokenSaved(options) {
+    //on vérifie si la requete necessite un token JWT
+    if (options === 'GET') return options;
+
+    try {
+        const data = fs.readFileSync('jwt.txt', 'utf8');
+        //On recupère le token JWT sauvegardé dans le fichier txt et on ajoute le champ Authorization dans les headers
+        options.headers['Authorization'] = 'Bearer ' + JSON.parse(data).token;
+
+    } catch (err) {
+        throw err;
+    }
+
+    return options;
+}
+
 
 testRequest();
